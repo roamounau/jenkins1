64dcb42d1c79faae15c6d8deef998f35e172f728
roamounau <gaipgaip55@gmail.com>
2024-10-29 10:50:56 +0900

    sd

---
+++ src/main/java/com/test/jenkins1/Jenkins1Application.java
@@ -0,0 +1,13 @@
+package com.test.jenkins1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jenkins1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jenkins1Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/test/jenkins1/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.test.jenkins1;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Jenkins1Application.class);
+	}
+
+}
